28276bc6bda9ab9aa66649eef4bed2dd9bab8812
¨Telios4¨ <davidteliosm@gmail.com>
2025-11-20 15:01:52 +0300

    Routes Created

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('admin', function () {
+    return view('admin');
+});
+Route::get('bookcatalogue', function () {
+    return view('bookcatalogue');
+});
+Route::get('profile', function () {
+    return view('profile');
+});
+Route::get('settings', function () {
+    return view('settings');
+});
+
